6ffe125f0accaa8e284b861381bb6f4933102b4c
Neha Sri <165380426+Alien0608@users.noreply.github.com>
2024-04-02 23:16:08 +0530

    Add files via upload
    simple login and signup page

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import  LoginForm  from './Component/LoginForm/LoginForm.jsx';
+
+function App() {
+  return (
+    
+    <div >
+        <LoginForm/>  
+    </div>
+  );
+}
+
+export default App;
